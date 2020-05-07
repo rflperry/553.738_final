782c08d14bc51339f23ccd2418e2a435c221ae7d
Christian Cosgrove <ccosgro2@jhu.edu>
2020-05-07 11:00:32 -0400

    immunity decay

--- test_sir.py
+++ test_sir.py
@@ -6,22 +6,23 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # Total population, N.
-N = 1000
+N = 1
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0, R0 = 0.1, 0.1
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
+# zeta = exponential decay rate for immunity
+beta, gamma, zeta = 0.5, 0.1, 0.01
 # A grid of time points (in days)
 t = np.linspace(0, 160, 160)
 
 # The SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
+def deriv(y, t, N, beta, gamma, zeta):
     S, I, R = y
-    dSdt = -beta * S * I / N
+    dSdt = -beta * S * I / N + zeta * R
     dIdt = beta * S * I / N - gamma * I
-    dRdt = gamma * I
+    dRdt = gamma * I - zeta * R
     return dSdt, dIdt, dRdt
 
 # Initial conditions vector
@@ -58,12 +59,13 @@ plt.clf()
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
 
-I0_range = np.logspace(0, 3, 50)
+# I0_range = np.logspace(-2, 0, 50)
+I0_range = np.linspace(0.02, 0.3, 20)
 
 for I0 in tqdm(I0_range):
-    y0 = S0, I0, R0
+    y0 = N-I0 - R0, I0, R0
     # Integrate the SIR equations over the time grid, t.
-    ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+    ret = odeint(deriv, y0, t, args=(N, beta, gamma, zeta))
     S, I, R = ret.T
 
     ax.plot(S, I, c='g')
@@ -74,3 +76,27 @@ plt.ylabel('Infected')
 plt.savefig('phaseportrait_S_I.png')
 
 
+
+
+plt.clf()
+# # Load case data
+
+# import csv
+
+# reader = csv.reader(open('cases.csv','r'))
+# total_cases = {}
+
+# c = 0
+
+# for line in reader:
+#     c+=1
+#     if c == 1:
+#         continue
+#     if line[0] not in total_cases:
+#         total_cases[line[0]] = 0
+#     total_cases[line[0]] += int(line[3])
+
+#     c+=1
+
+# plt.plot(np.arange(len(total_cases)), list(total_cases.values()))
+# plt.savefig('total_cases.png')
